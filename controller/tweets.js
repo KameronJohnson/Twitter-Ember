2e401c5465993959583fb7b5952f53aff17ddb40
Frank Meza and Kameron Johnson <KameronJohnson@gmail.com>
2015-03-03 15:40:09 -0800

    add date functionality

---
+++ controller/tweets.js
@@ -0,0 +1,17 @@
+Twitter.TweetsController = Ember.ArrayController.extend({
+
+  sortProperties: ['date'],
+  sortAscending: true,
+
+  actions: {
+    addTweet: function() {
+      var id = (tweets.length + 1).toString();
+      tweets.addObject({id: id, msg: this.msg, date: new Date().toLocaleString()});
+      this.set("msg", null);
+    },
+
+    reverseSort: function() {
+      this.set('sortAscending', !this.get('sortAscending'));
+    }
+  }
+});
