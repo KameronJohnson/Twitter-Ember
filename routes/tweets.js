6a2a764bf815e0d0cb278dce176405450794f4aa
Frank Meza and Kameron Johnson <KameronJohnson@gmail.com>
2015-03-03 12:16:18 -0800

    add notes

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,9 +1,11 @@
 Twitter.TweetsRoute = Ember.Route.extend({
   controllerName: 'tweets',
   renderTemplate: function() {
+    //tweets.hbs
     this.render('tweets');
   },
   model: function() {
+    //return the tweets array in store.js
     return tweets;
   }
 });
